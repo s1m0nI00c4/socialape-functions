14497d38059c5f234c44295992a52433659c05e5
Simonluca <43318934+s1m0nI00c4@users.noreply.github.com>
2021-01-18 19:50:32 +0100

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    apiKey: "AIzaSyCaAEGEmfMJ4FtqJdJA5yKIbeoIJAZTJBU",
-    authDomain: "socialape-8a473.firebaseapp.com",
-    projectId: "socialape-8a473",
-    storageBucket: "socialape-8a473.appspot.com",
-    messagingSenderId: "322551719600",
-    appId: "1:322551719600:web:d0e956b178c8eab7cdbd75",
-    measurementId: "G-306Z58WWBF"
+
   };
